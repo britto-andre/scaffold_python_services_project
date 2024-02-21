46a5ae577d3f1c1113cb33008651e712369e4103
André Britto <britto.abalf@gmail.com>
2024-02-20 22:41:25 -0300

    fix field excluded name

--- src/app/common/entity/deafult_entity.py
+++ src/app/common/entity/deafult_entity.py
@@ -7,13 +7,13 @@ PyObjectId = Annotated[str, BeforeValidator(str)]
 class DefaultEntity (BaseModel):
     id: Optional[PyObjectId] = Field(alias='_id', default=None)
     
-    exclude: bool = Field(default=None)
+    excluded: bool = Field(default=None)
     created_time: datetime = Field(default=None)
     # created_user: str = Field(default=None)
     
     version: int = Field(default=None)
 
     def model_post_init(self, *args, **kwargs):
-        if self.exclude == None: self.exclude = False
+        if self.excluded == None: self.excluded = False
         if self.created_time == None: self.created_time = datetime.now()
         if self.version == None: self.version = 0
